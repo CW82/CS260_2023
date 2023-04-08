fa68214233aa6d4160344fa7f6cd9b2761175dd7
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-04-07 22:06:34 -0700

    MarbleBag

---
+++ Assignment1/marblebag.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+
+using std:: cout;
+using std:: cin;
+
+struct marble {
+
+	int marb = 0, marbinbag = 0, bag = 0;
+	char marbcolor[10], marbsize[10];
+
+};
+
+int main() {
+
+	int usrinp;
+
+	printf("What would you like to do?\n");
+	printf(" 1: Add Marble\n 2: Delete Marble\n 3: Marbles in Bag Currently\n");
+
+	
+
+	return 0;
+}
