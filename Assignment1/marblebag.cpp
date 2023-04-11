290e4e5f65b680445a36287ab1a2341105117081
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-04-10 21:56:56 -0700

    Assignment1

--- Assignment1/marblebag.cpp
+++ Assignment1/marblebag.cpp
@@ -8,19 +8,43 @@ using std:: cin;
 
 struct marble {
 
-	int marb = 0, marbinbag = 0, bag = 0;
-	char marbcolor[10], marbsize[10];
+	int marb, marbinbag, bag;
+	char marbcolor, marbsize;
 
 };
 
 int main() {
 
+	marble starter;
+	starter.marb = 0;
+	starter.marbinbag = 0;
+	starter.bag = 0;
+
 	int usrinp;
 
 	printf("What would you like to do?\n");
-	printf(" 1: Add Marble\n 2: Delete Marble\n 3: Marbles in Bag Currently\n");
+	printf(" 1: Add Marble\n 2: Delete Marble\n 3: Marbles in Bag Currently\n 4: Desc of Marbles\n");
+	scanf("%d", &usrinp);
 
-	
+	if (usrinp == 1){
+
+		cout << "How many marbles would you like to add?: ";
+
+		scanf("%d", &starter.marb);
+
+		cout << "Marbles in Bag: " << starter.marb ++;
+
+	}
+
+	if (usrinp == 2){
+
+		cout << "How many marbles would you like to delete?: ";
+
+		scanf("%d", &starter.marb);
+
+		cout << "Marbles in Bag Now: " << starter.marb - starter.marb;
+
+	}
 
 	return 0;
 }
