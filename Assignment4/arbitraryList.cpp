570b3dd10a45bb8499a4114b30bc93a56a70807f
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-05-10 14:25:46 -0700

    arbitraryList

---
+++ Assignment4/arbitraryList.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using std::cout;
+using std::endl;
+using std::printf;
+
+struct Node{ 
+
+    int data; //struct for the LL. This is what the 'node' contains, integer data
+    Node *next; //pointer to the 'next' node
+
+};
