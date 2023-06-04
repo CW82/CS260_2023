65d52b726ce53d928f96acbce47056ae67095672
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-06-04 05:29:03 -0700

    BST

---
+++ Assignment6/BST.h
@@ -0,0 +1,15 @@
+
+
+struct BSTNode {
+
+    int data;
+    BSTNode* left;
+    BSTNode* right;
+
+    BSTNode(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+
+};
--- Assignment6/main.cpp
+++ Assignment6/main.cpp
@@ -1,91 +1,99 @@
 #include <iostream>
 
-using namespace std;
-
-#include <iostream>
+#include "BST.h"
 
 using namespace std;
-
-class BSTNode {
-public:
-    int data;
-    BSTNode* left;
-    BSTNode* right;
-
-    BSTNode(int value) {
-        data = value;
-        left = nullptr;
-        right = nullptr;
-    }
-};
 
 class BST {
 public:
-    BSTNode* root;
+    BSTNode *root;
 
-    void IOT(BSTNode* current) {
-        if (current != nullptr) {
-            IOT(current->left);
-            cout << current->data << " , ";
-            IOT(current->right);
-        }
+    BST() {
+        root = nullptr; // Initialize root to nullptr
     }
 
+    //In Order Traversal of the tree
+	void IOT(BSTNode *current) {
+		if (current->left != nullptr) { //if the current's left isn't NULL, go left
+			IOT(current->left);
+
+		}
+
+		cout << current->data << ", "; //print the current's data
+
+		if (current->right != nullptr) { //now go down the right side
+			IOT(current->right);
+
+		}
+	}
+
+    //add function to add to the BST where less than or equal to going to the left and greater than values going to the right
     void add(int data) {
-        root = addNode(root, data);
+        root = add(root, data); 
+
     }
 
-    BSTNode* addNode(BSTNode* node, int data) {
-        if (node == nullptr) {
-            return new BSTNode(data);
+    BSTNode *add(BSTNode *node, int data) { //pointer to the current node and the data that needs to be inserted
+        if (node == nullptr) { 
+            return new BSTNode(data); //traveres tree to find where to place data
+            
         }
 
-        if (data < node->data) {
-            node->left = addNode(node->left, data);
-        } else if (data > node->data) {
-            node->right = addNode(node->right, data);
+        if (data < node->data) { //if the data is less than the current node's data, go to the left
+            node->left = add(node->left, data);
+
+        } else if (data > node->data) { //if the data is larger than place the data on the right
+            node->right = add(node->right, data);
+			
         }
 
-        return node;
+        return node; //return the pointer of the current node
     }
 
-    void remove(int data) {
+    void remove(int data) { //takes 'data' for input
         root = removeNode(root, data);
     }
 
-    BSTNode* removeNode(BSTNode* node, int data) {
-        if (node == nullptr) {
+    BSTNode *removeNode(BSTNode *node, int data) { //pointer to the current node and the data that's going to be removed
+        if (node == nullptr) { //if the node to be remomved is NULL, then return null pointer
             return nullptr;
         }
 
-        if (data < node->data) {
-            node->left = removeNode(node->left, data);
+        if (data < node->data) { //if data is less than the current node's data
+            node->left = removeNode(node->left, data); //the data to be removed is to the elft 
+
         } else if (data > node->data) {
-            node->right = removeNode(node->right, data);
+            node->right = removeNode(node->right, data); //same thing, but on the right
+
+        //test cases
         } else {
             if (node->left == nullptr && node->right == nullptr) {
                 delete node;
-                node = nullptr;
-            } else if (node->left == nullptr) {
-                BSTNode* temp = node;
+                node = nullptr; //if the current node is nullptr, the leaf was reached and didn't find the data specified so return nullptr
+
+            } else if (node->left == nullptr) { //cases for if the node that's going to be deleted doesn't have 'children'
+                BSTNode* temp = node; 
                 node = node->right;
-                delete temp;
-            } else if (node->right == nullptr) {
+                delete temp; //delete the node and set to nullptr
+
+            } else if (node->right == nullptr) { //same, but on the right side
                 BSTNode* temp = node;
                 node = node->left;
                 delete temp;
+
             } else {
-                BSTNode* minNode = findMin(node->right);
+                BSTNode *minNode = findMin(node->right); //both have left and right children so find the min data on the right side
                 node->data = minNode->data;
-                node->right = removeNode(node->right, minNode->data);
+                node->right = removeNode(node->right, minNode->data); //replace current nodes data with data of the min value then remove
+
             }
         }
 
         return node;
     }
 
-    BSTNode* findMin(BSTNode* node) {
-        while (node->left != nullptr) {
+    BSTNode *findMin(BSTNode *node) { //to find the min data in the tree. takes the pointer to current node 
+        while (node->left != nullptr) { //iterates the left node until it gets the min data and returns a ptr to that min data node
             node = node->left;
         }
         return node;
@@ -95,85 +103,7 @@ public:
 int main() {
     BST tree;
 
-    // Add nodes to the BST
-    tree.add(40);
-    tree.add(20);
-    tree.add(60);
-    tree.add(10);
-    tree.add(30);
-    tree.add(50);
-    tree.add(70);
 
-    // Print the BST using inorder traversal
-    cout << "Inorder Traversal: ";
-    tree.IOT(tree.root);
-    cout << endl;
-
-    // Remove a node from the BST
-    tree.remove(30);
-
-    // Print the BST after removal
-    cout << "Inorder Traversal after removing 30: ";
-    tree.IOT(tree.root);
-    cout << endl;
 
     return 0;
 }
-
-
-/*struct Node {
-	int data;
-	Node *left = nullptr;
-	Node *right = nullptr;
-
-};
-
-class BSTNode {
-	public:
-		Node *root;
-
-		void add (Node *root, int data) {
-			if (root == NULL) {
-				return new Node (data);
-			}
-
-		}
-
-		int remove () {
-			if (root == NULL) {
-
-			}
-
-		}
-
-		void IOT (BTS *current) {
-				if (current->left != nullptr) {
-					IOT(current->left);
-
-				}
-				cout << current->data << " , ";
-
-				if (current->right != nullptr) {
-					IOT(current->right);
-
-				}
-
-			}
-};*/
-
-/*int main() {
-	auto *root = new BTSNode{40, nullptr, nullptr};
-
-	BTSNode *left = new BTSNode{20, nullptr, nullptr};
-	BTSNode *right = new BTSNode{60, nullptr, nullptr};
-
-	root->left = left;
-	root->right = right;
-
-    cout << "root->value: " << root->data << endl;
-    cout << "root->left->value: " << root->left->data << endl;
-    cout << "root->right->value: " << root->right->data;
-
-	return 0;
-
-}*/
