30163532181930bacc01ed35f869a4afb98d5702
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-06-04 23:50:03 -0700

    Update BST.h

--- Assignment6/BST.h
+++ Assignment6/BST.h
@@ -1,12 +1,10 @@
-
-
 struct BSTNode { //struct for the data to be enterered on the tree and the left and right pointers
 
     int data;
     BSTNode *left;
     BSTNode *right;
 
-    BSTNode(int value) {
+    BSTNode(int value) { //constructor, sets data to value and left and right to null so the nodes doesn't have any kids
         data = value;
         left = nullptr;
         right = nullptr;
