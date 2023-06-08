7e5919b625638efff0eab3aaea34dd2db84f1d4e
Crystal Wolf <103386835+CW82@users.noreply.github.com>
2023-06-08 10:32:43 -0700

    hashtable files

---
+++ Assignment7/HT.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+using std::string;
+
+class hashTable {
+	public:
+
+		hashTable (int initialCapacity = 17);
+		~hashTable ();
+
+		int add (string newKey);
+		string find (string key);
+		int getSize ();
+		int getCapacity ();
+	
+	private:
+	
+		int size;
+		int capacity;
+		string *values;
+		int hash (string key);
+
+};
